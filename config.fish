ece10a035bdece47ce1c3bc12fbffe71ed4752bd
Rahi Khan <rahikhan360@gmail.com>
2022-03-17 15:40:21 -0700

    added fish config file

---
+++ config.fish
@@ -0,0 +1,21 @@
+# no more annoying welcome message hehe
+set fish_greeting ""
+
+# to enable ANSI color codes
+set -gx TERM xterm-256color
+
+# aliases (life saver)
+alias c "clear"
+alias l "exa -l -a --icons"
+alias v "nvim"
+alias g "git"
+alias gs "git status"
+alias ga "git add ."
+alias gc "git commit"
+alias gp "git push"
+alias gpl "git pull"
+alias python "/usr/local/bin/python3.10"
+
+# prompt theme
+oh-my-posh prompt init fish --config '~/.poshthemes/takuya.omp.json' | source
+
