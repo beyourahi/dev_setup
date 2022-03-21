f3d5b59c7872ffdbab08a3218ddcd59d0b86c350
Rahi Khan <65137116+beyourahi@users.noreply.github.com>
2022-03-21 16:13:19 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -58,6 +58,8 @@ fisher install jethrokuan/z
 # Install node (current/latest)
 sudo apt remove --purge nodejs -y
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")" 
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 fisher install FabioAntunes/fish-nvm edc/bass
 nvm install node
 
