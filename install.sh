bc53c9e63b758217a90d5de6b4c9e68d32bd2fa7
Rahi Khan <65137116+beyourahi@users.noreply.github.com>
2022-03-21 16:04:49 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -35,7 +35,8 @@ rm -rf ~/exa
 sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
 sudo chmod +x /usr/local/bin/oh-my-posh
 mkdir ~/.poshthemes
-wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip -O ~/.poshthemes/themes.zip && unzip ~/.poshthemes/themes.zip -d ~/.poshthemes
+wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip -O ~/.poshthemes/themes.zip
+unzip ~/.poshthemes/themes.zip -d ~/.poshthemes
 chmod u+rw ~/.poshthemes/*.json
 rm ~/.poshthemes/themes.zip
 
