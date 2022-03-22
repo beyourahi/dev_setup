03a7ae76b6afe515d3e896f2cb625602dae20d5c
Rahi Khan <65137116+beyourahi@users.noreply.github.com>
2022-03-22 02:28:02 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -43,18 +43,6 @@ sudo make install
 cd ~/
 rm -rf ~/neovim
 
-# Install node (current/latest)
-sudo apt remove --purge nodejs -y
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-fisher install FabioAntunes/fish-nvm edc/bass jethrokuan/z
-nvm install node
-
-# Update yarn
-corepack enable
-yarn set version stable
-
 # Install VS Code
 sudo apt install -y code
 
