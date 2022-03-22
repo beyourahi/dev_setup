42e01b2b67c6b4d4a6bf07fc78c8029b5f975885
Rahi Khan <65137116+beyourahi@users.noreply.github.com>
2022-03-22 16:13:31 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -53,7 +53,6 @@ sudo apt remove --purge nodejs -y
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-fisher install FabioAntunes/fish-nvm edc/bass jethrokuan/z
 nvm install node
 
 # Update yarn
