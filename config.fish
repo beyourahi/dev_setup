b40cf735ae34f09623cbca08b82e61cc46332871
Rahi Khan <rahikhan360@gmail.com>
2022-03-25 04:50:56 +0600

    updated fish config path

--- config.fish
+++ config.fish
@@ -7,10 +7,19 @@ set -gx TERM xterm-256color
 # set default editor to Neovim
 set -gx EDITOR nvim
 
+set -gx PATH bin $PATH
+set -gx PATH ~/bin $PATH
+set -gx PATH ~/.local/bin $PATH
+set -gx PATH ~/.nvm/versions/node/v17.7.2/bin $PATH
+
+# NodeJS
+set -gx PATH node_modules/.bin $PATH
+
 # aliases (life saver)
 alias c "clear"
+alias r "ranger"
 alias l "exa -l -a --icons"
-alias v "nvim"
+alias v "lvim"
 alias g "git"
 alias gs "git status"
 alias ga "git add ."
@@ -22,3 +31,4 @@ alias python "/usr/local/bin/python3.10"
 
 # prompt theme
 oh-my-posh prompt init fish --config '~/.poshthemes/takuya.omp.json' | source
+
