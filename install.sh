d168e8690e31eab4dc2ed19847bc2aa8d571f093
Rahi Khan <65137116+beyourahi@users.noreply.github.com>
2023-02-28 07:29:34 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,8 +17,8 @@ sudo cp -r ~/exa/target/release/exa /usr/local/bin/
 rm -rf ~/exa
 
 # Install node (current/latest)
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+export NVM_DIR="$([ -z "$XDG_CONFIG_HOME-" ] && printf %s "$HOME/.nvm" || printf %s "$XDG_CONFIG_HOME/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm install --lts
 curl -qL https://www.npmjs.com/install.sh | sh
