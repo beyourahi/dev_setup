588312541b69c86612ee365363c6df0a1c1a4466
Rahi Khan <65137116+beyourahi@users.noreply.github.com>
2023-07-28 06:50:59 -0700

    Update aliases.sh

--- .config/windows/aliases.sh
+++ .config/windows/aliases.sh
@@ -8,38 +8,24 @@ alias c="clear"
 alias l="ls -la"
 
 # npm
-alias ni="npm install"
-alias nid="npm install -D"
+alias ni="npm install && npx prettier --write ."
+alias nid="npm install -D && npx prettier --write ."
 alias nd="npx prettier --write . && npm run dev"
 alias nb="npx prettier --write . && npm run build"
+alias ns="npm run start"
 alias nu="ncu && ncu -u && npm install && npx prettier --write ."
 
 # git
 alias g="git"
-alias gs="git status"
-alias gl="git log --oneline"
-alias ga="git add ."
+alias gs="npx prettier --write . && git status"
+alias gl="npx prettier --write . && git log --oneline"
+alias ga="npx prettier --write . && git add ."
 alias gc="git commit -m"
-alias gam="git commit --amend"
+alias gam="npx prettier --write . && git commit --amend"
 alias gp="git push --force"
-alias gpl="git pull"
-alias fuckit="git add . && git commit --amend && git push --force"
+alias gpl="git pull && npx prettier --write ."
+alias fuckit="git add . && npx prettier --write . && git commit --amend && git push --force"
 
 # prettier
 alias p="npx prettier --write ."
 alias pc="npx prettier --check ."
-
-case "$TERM" in
-xterm*)
-	# The following programs are known to require a Win32 Console
-	# for interactive usage, therefore let's launch them through winpty
-	# when run inside `mintty`.
-	for name in node ipython php php5 psql python2.7 winget
-	do
-		case "$(type -p "$name".exe 2>/dev/null)" in
-		''|/usr/bin/*) continue;;
-		esac
-		alias $name="winpty $name.exe"
-	done
-	;;
-esac
