212e7024700bcd3e6741a982cd55ad7d3662cff8
Rahi Khan <65137116+beyourahi@users.noreply.github.com>
2023-08-15 13:02:04 +0600

    Update aliases.sh

--- .config/windows/aliases.sh
+++ .config/windows/aliases.sh
@@ -9,23 +9,31 @@ alias l="ls -la"
 
 # npm
 alias ni="npm install && npx prettier --write ."
-alias nid="npm install -D && npx prettier --write ."
 alias nd="npx prettier --write . && npm run dev"
 alias nb="npx prettier --write . && npm run build"
 alias ns="npm run start"
 alias nu="ncu && ncu -u && npm install && npx prettier --write ."
 
+# pnpm
+alias px="pnpm dlx"
+alias pi="pnpm install && pnpm dlx prettier --write ."
+alias pa="pnpm add"
+alias pd="pnpm dlx prettier --write . && pnpm run dev"
+alias pb="pnpm dlx prettier --write . && pnpm run build"
+alias ps="pnpm run start"
+alias pu="ncu && ncu -u && pnpm install && pnpm dlx prettier --write ."
+
 # git
 alias g="git"
-alias gs="npx prettier --write . && git status"
-alias gl="npx prettier --write . && git log --oneline"
-alias ga="npx prettier --write . && git add ."
+alias gs="pnpm dlx prettier --write . && git status"
+alias gl="pnpm dlx prettier --write . && git log --oneline"
+alias ga="pnpm dlx prettier --write . && git add ."
 alias gc="git commit -m"
-alias gam="npx prettier --write . && git commit --amend"
+alias gam="pnpm dlx prettier --write . && git commit --amend"
 alias gp="git push --force"
-alias gpl="git pull && npx prettier --write ."
-alias fuckit="git add . && npx prettier --write . && git commit --amend && git push --force"
+alias gpl="git pull && pnpm dlx prettier --write ."
+alias fuckit="git add . && pnpm dlx prettier --write . && git commit --amend && git push --force"
 
 # prettier
-alias p="npx prettier --write ."
-alias pc="npx prettier --check ."
+alias p="pnpm dlx prettier --write ."
+alias pc="pnpm dlx prettier --check ."
