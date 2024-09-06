96a50f4c978ad7a04ac21fc59b1bf455c9af8be3
Rahi Khan <beyourahi@gmail.com>
2024-09-07 05:02:56 +0600

    Update aliases.sh

--- .config/windows/aliases.sh
+++ .config/windows/aliases.sh
@@ -1,6 +1,25 @@
-# aliases (life saver)
+# Some good standards, which are not used if the user
+# creates his/her own .bashrc/.bash_profile
 
-# C:\Program Files\Git\etc\profile.d
+# --show-control-chars: help showing Korean or accented characters
+alias ls='ls -F --color=auto --show-control-chars'
+alias ll='ls -l'
+
+case "$TERM" in
+xterm*)
+	# The following programs are known to require a Win32 Console
+	# for interactive usage, therefore let's launch them through winpty
+	# when run inside `mintty`.
+	for name in node ipython php php5 psql python2.7 winget
+	do
+		case "$(type -p "$name".exe 2>/dev/null)" in
+		''|/usr/bin/*) continue;;
+		esac
+		alias $name="winpty $name.exe"
+	done
+	;;
+esac
+
 
 # basic
 alias e="exit"
@@ -8,13 +27,6 @@ alias t="touch"
 alias rm="rm -rf"
 alias c="clear"
 alias l="ls -la"
-
-# npm
-alias ni="npm install && npx prettier --write ."
-alias nd="npx prettier --write . && npm run dev"
-alias nb="npx prettier --write . && npm run build"
-alias ns="npm run start"
-alias nu="ncu && ncu -u && npm install && npx prettier --write ."
 
 # pnpm
 alias px="pnpm dlx"
@@ -25,19 +37,29 @@ alias pb="pnpm dlx prettier --write . && pnpm run build"
 alias ps="pnpm run start"
 alias pu="ncu && ncu -u && pnpm install && pnpm dlx prettier --write ."
 
+# bun
+alias bx="bunx"
+alias bi="bun install && bunx prettier --write ."
+alias ba="bun add"
+alias br="bun remove"
+alias bu="bun update --latest"
+alias bd="bunx prettier --write . && bun run dev"
+alias bb="bunx prettier --write . && bun run build"
+alias bs="bun run start"
+
 # git
 alias g="git"
-alias gs="pnpm dlx prettier --write . && git status"
-alias gl="pnpm dlx prettier --write . && git log --oneline"
-alias ga="pnpm dlx prettier --write . && git add ."
+alias gs="bunx prettier --write . && git status"
+alias gl="bunx prettier --write . && git log --oneline"
+alias ga="bunx prettier --write . && git add ."
 alias gc="git commit -m"
-alias gam="pnpm dlx prettier --write . && git commit --amend"
+alias gam="bunx prettier --write . && git commit --amend"
 alias gp="git push --force"
-alias gpl="git pull && pnpm dlx prettier --write ."
+alias gpl="git pull && bunx prettier --write ."
 alias gb="git branch"
 alias gck="git checkout"
-alias fuckit="git add . && pnpm dlx prettier --write . && git commit --amend && git push --force"
+alias fuckit="git add . && bunx prettier --write . && git commit --amend && git push --force"
 
 # prettier
-alias p="pnpm dlx prettier --write ."
-alias pc="pnpm dlx prettier --check ."
+alias p="bunx prettier --write ."
+alias pc="bunx prettier --check ."
